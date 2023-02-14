bd1933f362dda67a7bbe152cd2831ee4e6871fb1
AbhijeetVashishtha <82046366+AbhijeetVashishtha@users.noreply.github.com>
2023-02-14 23:06:59 +0530

    Delete DaytoDay Expense using NoSQL directory

--- DaytoDay Expense using NoSQL/routes/premiumuser.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const premiumUserController = require('../controllers/premiumuser');
-const authenticatemiddleware = require('../middleware/auth');
-const router = express.Router();
-
-router.get('/showLeaderBoard',authenticatemiddleware.authenticate, premiumUserController.getUserLeaderBoard);
-
-module.exports = router;
